ae955ca480d368a776f9858155f75d8dde3191f7
bg-gif <goff.ben@gmail.com>
2019-12-06 11:06:34 +0000

    migrations and server setup

---
+++ backend/db/migrations/20191206104459_vendors.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('vendors', table => {
+    table.increments('vendor_id');
+    table.string('vendorname').notNullable();
+    table.string('ownername').notNullable();
+    table.string('phone_num');
+    table.string('cuisine').notNullable();
+    table.string('email');
+    table.string('menu');
+    table.string('opening_times').notNullable();
+    table.string('location').notNullable();
+    table.boolean('open_status');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('vendors');
+};
