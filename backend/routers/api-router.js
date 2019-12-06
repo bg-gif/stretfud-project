6a5d78ee28cabcb59eb51b856c42aba99df94615
bg-gif <goff.ben@gmail.com>
2019-12-06 14:31:20 +0000

    get user by userid endpoint added

--- backend/routers/api-router.js
+++ backend/routers/api-router.js
@@ -1,4 +1,7 @@
-const apiRouter = require('express').Router();
+const apiRouter = require('express')();
 const { handle405s } = require('../errors/index');
+const usersRouter = require('../routers/users-router');
+
+apiRouter.use('/users', usersRouter);
 
 module.exports = apiRouter;
--- backend/spec/server.spec.js
+++ backend/spec/server.spec.js
@@ -23,7 +23,30 @@ describe('server', () => {
     describe('/users', () => {
       describe('/user_id', () => {
         describe('GET', () => {
-          it('status 200: returns an object with key of user and expected value', () => {});
+          it('status:200, returns an object with key of user and expected value', () => {
+            return request
+              .get('/api/users/1')
+              .expect(200)
+              .then(({ body }) => {
+                expect(body).to.be.an('object');
+                expect(body).to.contain.keys(
+                  'user_id',
+                  'username',
+                  'realname',
+                  'phone_num',
+                  'email',
+                  'age'
+                );
+              });
+          });
+          it.only('status:404, invalid user id', () => {
+            return request
+              .get('/api/users/mike')
+              .expect(404)
+              .then(({ body: { msg } }) => {
+                expect(msg).to.equal('Not Found');
+              });
+          });
         });
       });
     });
