daff91bb72a5205dae2c7f58931743037ee220b7
bg-gif <goff.ben@gmail.com>
2019-12-06 16:22:08 +0000

    fixed tests after table changes

--- backend/models/users-model.js
+++ backend/models/users-model.js
@@ -1,9 +1,9 @@
 const connection = require('../db/connection');
 
-exports.fetchUserById = user_id => {
+exports.fetchUserById = username => {
   return connection('users')
     .select('*')
-    .where({ user_id })
+    .where({ username })
     .then(response => {
       return response.length === 0
         ? Promise.reject({ status: 404, msg: 'User Does Not Exist' })
