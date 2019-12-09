3a6fac70c51266c2812f3b6fe629491cc2cec451
bg-gif <goff.ben@gmail.com>
2019-12-09 14:52:57 +0000

    stop sending passwords

--- models/users-model.js
+++ models/users-model.js
@@ -1,20 +1,20 @@
-const connection = require("../db/connection");
+const connection = require('../db/connection');
 
 exports.fetchUserById = username => {
-  return connection("users")
-    .select("*")
+  return connection('users')
+    .select('username', 'realname', 'phone_num', 'email', 'age')
     .where({ username })
     .then(response => {
       return response.length === 0
-        ? Promise.reject({ status: 404, msg: "User Does Not Exist" })
+        ? Promise.reject({ status: 404, msg: 'User Does Not Exist' })
         : response;
     });
 };
 
 exports.postUserMod = user => {
-  return connection("users")
+  return connection('users')
     .insert(user)
-    .returning("*")
+    .returning('username', 'realname', 'phone_num', 'email', 'age')
     .then(([user]) => {
       return user;
     });
@@ -22,11 +22,11 @@ exports.postUserMod = user => {
 
 exports.updateUserByUsername = (username, update) => {
   const { phone_num, email } = update;
-  return connection("users")
+  return connection('users')
     .where({ username })
     .modify(query => {
       if (phone_num) query.update({ phone_num });
       if (email) query.update({ email });
     })
-    .returning("*");
+    .returning('*');
 };
--- models/vendors-model.js
+++ models/vendors-model.js
@@ -1,37 +1,80 @@
-const knex = require("../db/connection");
+const knex = require('../db/connection');
 
 exports.fetchVendors = () => {
-  return knex("vendors")
-    .select("*")
-    .returning("*")
+  return knex('vendors')
+    .select(
+      'username',
+      'ownername',
+      'cuisine',
+      'location',
+      'opening_times',
+      'open_status',
+      'menu',
+      'businessname',
+      'phone_num',
+      'email'
+    )
     .then(vendors => {
       return vendors;
     });
 };
 
 exports.fetchVendorByUsername = Username => {
-  return knex("vendors")
-    .select("*")
-    .where({ "vendors.username": Username })
+  return knex('vendors')
+    .select(
+      'username',
+      'ownername',
+      'cuisine',
+      'location',
+      'opening_times',
+      'open_status',
+      'menu',
+      'businessname',
+      'phone_num',
+      'email'
+    )
+    .where({ 'vendors.username': Username })
     .then(vendor => {
       return vendor;
     });
 };
 
 exports.sendVendor = user => {
-  return knex("vendors")
+  return knex('vendors')
     .insert(user)
-    .returning("*");
+    .returning(
+      'username',
+      'ownername',
+      'cuisine',
+      'location',
+      'opening_times',
+      'open_status',
+      'menu',
+      'businessname',
+      'phone_num',
+      'email'
+    );
 };
 
 exports.patchVendor = (update, username) => {
   const { location, open_status, menu } = update;
-  return knex("vendors")
+  return knex('vendors')
     .where({ username })
     .modify(query => {
       if (location) query.update({ location });
       if (open_status) query.update({ open_status });
       if (menu) query.update({ menu });
     })
-    .returning("*");
+    .returning(
+      'username',
+      'ownername',
+      'cuisine',
+      'location',
+      'opening_times',
+      'open_status',
+      'menu',
+      'businessname',
+      'phone_num',
+      'email'
+    );
 };
