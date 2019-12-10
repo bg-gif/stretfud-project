c4fbe4ceac82cd441dde678af8721945c597581e
bg-gif <goff.ben@gmail.com>
2019-12-10 10:10:29 +0000

    tweaking aws

--- routers/upload-router.js
+++ routers/upload-router.js
@@ -4,14 +4,14 @@ const AWS = require('aws-sdk');
 const { S3_SECRET, S3_KEY, S3_BUCKET_NAME } = process.env;
 
 const s3 = new AWS.S3({
-  accessKeyId: ID,
-  secretAccessKey: SECRET
+  accessKeyId: S3_KEY,
+  secretAccessKey: S3_SECRET
 });
 
 const uploadFile = filename => {
   const fileContent = fs.readFileSync('');
   const params = {
-    Bucket: BUCKET_NAME,
+    Bucket: S3_BUCKET_NAME,
     Key: 'test.jpg',
     Body: fileContent
   };
