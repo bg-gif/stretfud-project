0ceed4dc0e4c67dc8af4c857ad2038eee97600ee
waivey <waivey@gmail.com>
2019-12-10 15:41:34 +0000

    refactoring merge of utils file

--- stretfudProj/utils/utils.js
+++ stretfudProj/utils/utils.js
@@ -1,28 +1,3 @@
-const axios = require("axios");
-const base_URL = "https://stretfud.herokuapp.com/api";
-
-exports.fetchVendor = username => {
-  return axios.get(`${base_URL}/vendors/${username}`).then(({ data }) => {
-    return data.vendor;
-  });
+exports.formatLocation = ({ latitude, longitude }) => {
+  return `${latitude}, ${longitude}`;
 };
-
-exports.updateVendorInfo = ({ username, location, open_status, menu }) => {
-  return axios
-    .patch(`${base_URL}/vendors/${username}`, { open_status })
-    .then(({ data }) => {
-      console.log(data.vendor);
-      return data.vendor;
-    })
-    .catch(err => {
-      console.log(err, "<<< error");
-    });
-};
-
-// exports.fetchVendorsByLocation = (lat, long) => {
-//   return axios
-//     .get(`${base_URL}/vendors?=${lat},${long}`)
-//     .then(({ data: { vendors } }) => {
-//       return vendors;
-//     });
-// };
