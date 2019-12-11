0ce42220e1ef6fbda44ba987835fa276c6da2b58
James Stevenson <jamesstevenson@laurens-MacBook-Air.local>
2019-12-11 14:44:34 +0000

    style the input boxes to show users the appropriate values.

--- stretfudProj/components/InputAdder.js
+++ stretfudProj/components/InputAdder.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Text, TextInput, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  InputAccessoryView
+} from 'react-native';
 
 const InputAdder = ({ name, handleTextChange, value }) => {
   const handleChange = text => {
     handleTextChange(text, name);
   };
+  const inputAccessoryViewID = name;
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{name}</Text>
       <TextInput
+        inputAccessoryViewID={inputAccessoryViewID}
         onChangeText={handleChange}
         style={styles.inputBox}
         secureTextEntry={name === 'password'}
         value={value}
       ></TextInput>
-      {/* {this.state.errorMsg && <Text>Please enter some deets</Text>} */}
+      <InputAccessoryView nativeID={inputAccessoryViewID}>
+        <View style={styles.keyboardView}>
+          <Text style={styles.inputAccessoryViewIDText}>{name}: </Text>
+          <TextInput
+            value={value}
+            secureTextEntry={name === 'password'}
+            style={styles.inputAccessoryViewIDValue}
+          ></TextInput>
+        </View>
+      </InputAccessoryView>
     </View>
   );
 };
@@ -40,6 +57,28 @@ const styles = StyleSheet.create({
     color: 'rgba(198, 197, 185, 1)',
     fontSize: 20,
     fontFamily: 'BebasNeue-Regular'
+  },
+  inputAccessoryViewIDText: {
+    fontWeight: '400',
+    fontSize: 20,
+    textAlign: 'center',
+    width: 150,
+    paddingTop: 10
+  },
+  inputAccessoryViewIDValue: {
+    fontWeight: '400',
+    fontSize: 20,
+    textAlign: 'center',
+    width: 150,
+    paddingTop: 10,
+    textAlign: 'left'
+  },
+  keyboardView: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(198, 197, 185, 1)',
+    height: 40
   }
 });
 
