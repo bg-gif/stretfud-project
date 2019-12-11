056e5e43d3d3bbf5c324dce54aa6abc8b25ace04
waivey <waivey@gmail.com>
2019-12-11 16:46:49 +0000

    styling started on VendorMenuCard

--- stretfudProj/components/VendorMenuCard.js
+++ stretfudProj/components/VendorMenuCard.js
@@ -7,19 +7,44 @@ const VendorMenuCard = ({ menuItem, handleSwitch }) => {
   };
 
   return (
-    <View>
-      <Text>
-        {menuItem.item} £{menuItem.price}
-      </Text>
-      <Text>{menuItem.description}</Text>
-      <Text>{menuItem.available}</Text>
-      <Switch
-        onValueChange={handleAvailability}
-        name={menuItem.item}
-        value={menuItem.available}
-      />
+    <View style={styles.menuCard}>
+      <View style={styles.menuDetails}>
+        <Text>
+          {menuItem.item} £{menuItem.price}
+        </Text>
+        <Text>{menuItem.description}</Text>
+        <Text>{menuItem.available}</Text>
+      </View>
+
+      <View style={styles.availabilityButtonContainer}>
+        <Text>Available:</Text>
+        <Switch
+          onValueChange={handleAvailability}
+          name={menuItem.item}
+          value={menuItem.available}
+        />
+      </View>
     </View>
   );
 };
 
 export default VendorMenuCard;
+
+const styles = StyleSheet.create({
+  menuCard: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  menuDetails: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 2
+  },
+  availabilityButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: 20
+  }
+});
