51868754c3b9ccaf0437c60bd4f0f30c06621163
bg-gif <goff.ben@gmail.com>
2019-12-13 09:33:36 +0000

    testing

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -1,7 +1,8 @@
 const uploadFile = require('../models/upload-model');
 
 exports.sendPhoto = (req, res, next) => {
-  const { uri } = req.body;
+  console.log('called');
+  const uri = req.body;
   console.log(uri);
   uploadFile(uri).then(response => {
     res.status(200).send('confirmed');
