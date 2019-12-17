6958afb7cbfd566e85a445b9b7caba16f3370f59
bg-gif <goff.ben@gmail.com>
2019-12-17 12:10:55 +0000

    default status to pending on seed

--- db/migrations/20191216143813_orders.js
+++ db/migrations/20191216143813_orders.js
@@ -1,21 +1,21 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("orders", table => {
-    table.increments("order_id");
+  return knex.schema.createTable('orders', table => {
+    table.increments('order_id');
     table
-      .string("user_username")
-      .references("username")
-      .inTable("users")
+      .string('user_username')
+      .references('username')
+      .inTable('users')
       .notNullable();
     table
-      .string("vendor_username")
-      .references("username")
-      .inTable("vendors")
+      .string('vendor_username')
+      .references('username')
+      .inTable('vendors')
       .notNullable();
-    table.string("status");
-    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string('status').defaultTo('pending');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("orders");
+  return knex.schema.dropTableIfExists('orders');
 };
